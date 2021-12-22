7c1386102c8fbbd6640d97c4754e42cd2f29ccb1
Andrei Chenchik <andrei@chenchik.me>
2021-12-22 12:09:46 +0100

    System setup script

---
+++ .scripts/macos_setup.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+echo "Make sure you are logged in into AppStore already"
+read -p "Press enter to continue"
+echo "macOS setup is in progress!"
+
+# Prepare Xcode
+xcode-select --install
+sudo softwareupdate --install-rosetta
+sudo xcodebuild -license accept
+
+# Setup brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+eval "$(/opt/homebrew/bin/brew shellenv)"
+brew update
+brew bundle install -f $HOME/Brewfile
+
+# Install ohmyzsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Setup git
+git config --global user.name "Andrei Chenchik"
+git config --global user.email andrei@chenchik.me
+
+# Restore configs
+dotfiles_dir=".dotfiles"
+git clone --bare <git-repo-url> $HOME/$dotfiles_dir
+alias config='git --git-dir=$HOME/$dotfiles_dir/ --work-tree=$HOME'
+mkdir -p .config-backup && \
+config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | \
+xargs -I{} mv {} .config-backup/{}
+config checkout
+config config --local status.showUntrackedFiles no
+
+# macOS Config
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 0
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+defaults write com.apple.Finder AppleShowAllFiles true
+killall Finder
+
+# 1Password Login
+echo "Enter your 1password email: "
+read user_email
+op signin my.1password.com $user_email                          
+
+echo "Done, please restart terminal session"
